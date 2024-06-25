fdb7db100f2a4240268041a32192aadb7a09e3ff
G0409M <gabrysia.maslanka@gmail.com>
2024-06-25 07:46:14 +0200

    Dto's i IServices

---
+++ Projekt/Services/IMovieRoleService.cs
@@ -0,0 +1,18 @@
+﻿using Projekt.Dto.MovieRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Services
+{
+    public interface IMovieRoleService
+    {
+        List<MovieRoleDto> GetAll();
+        MovieRoleDto GetById(int id);
+        int Create(CreateMovieRoleDto dto);
+        void Update(UpdateMovieRoleDto dto);
+        void Delete(int id);
+    }
+}
---
+++ Projekt/Services/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿using Projekt.Dto.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Services
+{
+    public interface IMovieService
+    {
+        List<MovieDto> GetAll();
+        MovieDto GetById(int id);
+        int Create(CreateMovieDto dto);
+        void Update(UpdateMovieDto dto);
+        void Delete(int id);
+    }
+}
