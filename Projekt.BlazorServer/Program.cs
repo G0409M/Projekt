58497f9c61a19e237b7bb3e6ad8348ce5392e9a2
G0409M <gabrysia.maslanka@gmail.com>
2024-06-25 17:34:24 +0200

    Movies and Actors Page

--- Projekt.BlazorServer/Program.cs
+++ Projekt.BlazorServer/Program.cs
@@ -1,6 +1,19 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Projekt.Application.Mappings;
+using Projekt.Application.Services;
+using Projekt.Application.Validators;
 using Projekt.BlazorServer.Data;
+using Projekt.Domain.Contracts;
+using Projekt.SharedKernel.Dto.Movie;
+using Microsoft.EntityFrameworkCore;
+using Projekt.Infrastructure;
+using Projekt.Infrastructure.Repositories;
+using Projekt.SharedKernel.Dto;
+using NLog.Web;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +21,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+// Rejestracja AutoMapper
+builder.Services.AddAutoMapper(typeof(ProjektMapper));
+// FluentValidation waliduje i przekazuje wynik przez ModelState
+builder.Services.AddFluentValidationAutoValidation();
+// Rejestracja kontekstu bazy w kontenerze IoC
+var sqliteConnectionString = "Data Source=Projekt.WebAPI.db";
+builder.Services.AddDbContext<ProjektDbContext>(options =>
+    options.UseSqlite(sqliteConnectionString));
+
+builder.Services.AddScoped<IProjektUnitOfWork, ProjektUnitOfWork>();
+
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+builder.Services.AddScoped<IMovieRoleRepository, MovieRoleRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IMovieService, MovieService>();
+builder.Services.AddScoped<IMovieRoleService, MovieRoleService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+//builder.Services.AddScoped<ExceptionMiddleware>();
+
+builder.Services.AddScoped<DataSeeder>();
+
+builder.Services.AddScoped<IValidator<CreateMovieDto>, RegisterCreateMovieDtoValidator>();
 
 var app = builder.Build();
 
