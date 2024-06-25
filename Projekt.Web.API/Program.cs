dcab0a9f977787f2aac07b5cee22b64d9f1f6fb4
Kamil <husakkamil@student.agh.edu.pl>
2024-06-25 19:04:17 +0200

    Logger

--- Projekt.Web.API/Program.cs
+++ Projekt.Web.API/Program.cs
@@ -17,80 +17,98 @@ using Projekt.Infrastructure;
 using Projekt.Infrastructure.Repositories;
 using Projekt.Web.API.Middleware;
 using Projekt.Application.Mappings;
+using NLog.Web;
+using NLog;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-
         // Add services to the container.
-
-        builder.Services.AddControllers();
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
+        var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+        logger.Debug("init main");
+        try
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Projekt API", Version = "v1" });
-        });
+            var builder = WebApplication.CreateBuilder(args);
+            // NLog: Setup NLog for Dependency injection
+            builder.Logging.ClearProviders();
+            builder.Host.UseNLog();
+            builder.Services.AddControllers();
 
-        // Rejestracja kontekstu bazy w kontenerze IoC
-        var sqliteConnectionString = "Data Source=Projekt.WebAPI.db";
-        builder.Services.AddDbContext<ProjektDbContext>(options =>
-            options.UseSqlite(sqliteConnectionString));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Projekt API", Version = "v1" });
+            });
 
-        builder.Services.AddScoped<IProjektUnitOfWork, ProjektUnitOfWork>();
+            // Rejestracja kontekstu bazy w kontenerze IoC
+            var sqliteConnectionString = "Data Source=Projekt.WebAPI.db";
+            builder.Services.AddDbContext<ProjektDbContext>(options =>
+                options.UseSqlite(sqliteConnectionString));
 
-        builder.Services.AddScoped<IMovieRepository, MovieRepository>();
-        builder.Services.AddScoped<IMovieRoleRepository, MovieRoleRepository>();
-        builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IProjektUnitOfWork, ProjektUnitOfWork>();
 
-        builder.Services.AddScoped<IMovieService, MovieService>();
-        builder.Services.AddScoped<IMovieRoleService, MovieRoleService>();
-        builder.Services.AddScoped<IReviewService, ReviewService>();
-        builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+            builder.Services.AddScoped<IMovieRoleRepository, MovieRoleRepository>();
+            builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-        builder.Services.AddScoped<ExceptionMiddleware>();
+            builder.Services.AddScoped<IMovieService, MovieService>();
+            builder.Services.AddScoped<IMovieRoleService, MovieRoleService>();
+            builder.Services.AddScoped<IReviewService, ReviewService>();
+            builder.Services.AddScoped<IUserService, UserService>();
 
-        builder.Services.AddScoped<DataSeeder>();
+            builder.Services.AddScoped<ExceptionMiddleware>();
 
-        builder.Services.AddScoped<IValidator<CreateMovieDto>, RegisterCreateMovieDtoValidator>();
+            builder.Services.AddScoped<DataSeeder>();
 
-        // Rejestracja AutoMapper
-        builder.Services.AddAutoMapper(typeof(ProjektMapper));
+            builder.Services.AddScoped<IValidator<CreateMovieDto>, RegisterCreateMovieDtoValidator>();
 
-        // Rejestracja automatycznej walidacji
-        // FluentValidation waliduje i przekazuje wynik przez ModelState
-        builder.Services.AddFluentValidationAutoValidation();
+            // Rejestracja AutoMapper
+            builder.Services.AddAutoMapper(typeof(ProjektMapper));
 
-        var app = builder.Build();
+            // Rejestracja automatycznej walidacji
+            // FluentValidation waliduje i przekazuje wynik przez ModelState
+            builder.Services.AddFluentValidationAutoValidation();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projekt API v1");});
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projekt API v1"); });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            //...
+            // uruchomienie seedera
+            using (var scope = app.Services.CreateScope())
+            {
+                var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+                dataSeeder.Seed();
+            }
+            app.Run();
+
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.UseMiddleware<ExceptionMiddleware>();
-
-        //...
-        // uruchomienie seedera
-        using (var scope = app.Services.CreateScope())
+        catch (Exception exception)
         {
-            var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
-            dataSeeder.Seed();
+            // NLog: catch setup errors
+            logger.Error(exception, "Stopped program because of exception");
+            throw;
         }
-        app.Run();
-
+        finally
+        {
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            NLog.LogManager.Shutdown();
+        }
     }
 }
