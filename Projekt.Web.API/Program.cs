0e48eef6faccb37df6efc2bbaaed9e6ead7cb837
Kamil <husakkamil@student.agh.edu.pl>
2024-06-25 16:04:06 +0200

    Usunięto błąd nieznajdowania strony

--- Projekt.Web.API/Program.cs
+++ Projekt.Web.API/Program.cs
@@ -72,7 +72,7 @@ class Program
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projekt API v1"); c.RoutePrefix = string.Empty; });
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projekt API v1");});
         }
 
         app.UseHttpsRedirection();
